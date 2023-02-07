d4b9699e57b7494d23b5ba54e155109b89cff20c
Anton <antybumer@gmail.com>
2023-02-07 11:45:59 +0300

    add icon animation

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -10,11 +10,15 @@
       </div>
     </div>
 
-    <div class="weather_icon">
-      <span class="weather_icon_contry">{{fetchDataResult.sys.country}}</span>
+    <div class="weather_iconModule">
+      <div class="img" v-bind:style="styleObject">
+        <span class="weather_iconModule_contry">{{fetchDataResult.sys.country}}</span>
+       
+        <Icon icon="material-symbols:toggle-on-outline" class="weather_iconModule_actionSwitcher" :class="{ off :!actionIcon}" @click="actionIcon=!actionIcon" height="20" />
+        <div class="weather_iconModule_contry_icon" :class="{active:actionIcon}">
+          <Icon :icon="img" height="40" />
+        </div>
 
-      <div class="img" v-bind:style="styleObject" >
-        <Icon :icon="img" height="40" />
         <!-- <img :src="img1" alt="icon" v-bind:style="styleObject" /> -->
       </div>
 
@@ -100,24 +104,26 @@ export default defineComponent({
     styleObject: Object
   },
   setup(props) {
-    let img1 = ref(`http://openweathermap.org/img/wn/${props.fetchDataResult.weather[0].icon}@2x.png`);
+    let img1 = ref(
+      `http://openweathermap.org/img/wn/${props.fetchDataResult.weather[0].icon}@2x.png`
+    );
     let img = computed((): string => {
       switch (props.fetchDataResult.weather[0].icon) {
         case "01d":
           return "mdi:weather-sunny";
-          case "01n":
+        case "01n":
           return "mdi:weather-sunny";
         case "02d":
           return "mdi:weather-partly-cloudy";
-          case "02n":
+        case "02n":
           return "mdi:weather-partly-cloudy";
         case "03d":
           return "mdi:weather-cloudy";
-          case "03n":
+        case "03n":
           return "mdi:weather-cloudy";
         case "04d":
           return "fluent:weather-cloudy-24-regular";
-          case "04n":
+        case "04n":
           return "fluent:weather-cloudy-24-regular";
         case "09d":
           return "material-symbols:weather-snowy-outline";
@@ -130,8 +136,8 @@ export default defineComponent({
         case "50d":
           return "mdi:weather-mist";
       }
-     
     });
+    let actionIcon = ref<boolean>(true);
 
     let clock = ref<string>("");
     function timer() {
@@ -148,12 +154,23 @@ export default defineComponent({
 
     let styleObject = ref(props.styleObject);
 
-    return { img, img1, styleObject, clock };
+    return { img, img1, styleObject, clock, actionIcon };
   }
 });
 </script>
 
 <style lang="scss" scoped>
+@keyframes activeIcon {
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+}
 .weather {
   color: rgb(255, 255, 255);
   background: rgb(0, 0, 0);
@@ -170,24 +187,43 @@ export default defineComponent({
   box-shadow: 1px 1px rgb(247, 3, 3), 0 0 0px 1px rgb(236, 236, 236),
     5px 5px rgb(192, 192, 192);
 
-  .weather_icon {
+  .weather_iconModule {
     position: relative;
     display: flex;
     align-items: center;
     gap: 10px;
     margin-bottom: 5px;
     .img {
+      position: relative;
       padding: 18px;
       background: rgba(18, 49, 205, 0);
       border-radius: 15px;
       box-shadow: 1px 1px rgb(247, 3, 3), 0 0 0px 1px rgb(236, 236, 236),
         5px 5px rgb(192, 192, 192), 5px 5px 0 1px rgba(255, 0, 0, 0.712);
+
+      .weather_iconModule_contry_icon {
+        &.active {
+          animation: activeIcon 3s infinite linear;
+        }
+      }
+     
+      .weather_iconModule_contry {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+      }
+      .weather_iconModule_actionSwitcher {
+        position: absolute;
+        bottom: 5px;
+        right: 5px;
+
+        &.off{
+          transform:rotateY(180deg)
+        }
+      }
+
     }
-    .weather_icon_contry {
-      position: absolute;
-      top: 5px;
-      left: 5px;
-    }
+    
   }
   .weather_header {
     font-size: 18px;
